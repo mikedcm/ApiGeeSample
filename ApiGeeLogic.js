ebb083e459f56b9b466b9be5c9e24d0d9be4c154
mikedcm <mihai_lit@yahoo.com>
2015-11-06 17:05:59 +0200

    first working queries

--- ApiGeeLogic.js
+++ ApiGeeLogic.js
@@ -1,16 +1,15 @@
-var client;
-//var myEntity={type:'person', name:'secondOne', first:'jane',last:'doe',job:'analyst'};
+var myclient;
 
 function LogIn()
 {
-  client = new Usergrid.Client({
+  myclient = new Usergrid.Client({
 	orgName:"MikePete",
 	appName:"sandbox"});
 }
 
 function createPerson(myEntity)
 {
-client.createEntity(myEntity, function(error, createdEntity) //creates an entity
+myclient.createEntity(myEntity, function(error, createdEntity) //creates an entity
 {
 	if(error){console.log('could not create entity');}
 	else {console.log("created entity "+createdEntity.get("uuid"));} //to retrieve entity properties
@@ -19,12 +18,10 @@ client.createEntity(myEntity, function(error, createdEntity) //creates an entity
 
 function displayQueryResult(people, displayEl)
 {
+  displayEl.innerHTML="";
   while(people.hasNextEntity())
   {
     var person=people.getNextEntity();
-	var props=person.get();
-	for(var pr in props)
-		displayEl.innerHTML+=props[pr];
     var first=person.get('first');
     var last=person.get('last');
     var job=person.get('job');
@@ -34,11 +31,43 @@ function displayQueryResult(people, displayEl)
 
 function queryPerson(queryOption,displayAt)
 {
-client.createCollection(queryOption, function(error,people)
+myclient.createCollection(queryOption, function(error,people)
 {
 	if(error) console.log('could not query collection');
 	else {		
 		displayQueryResult(people,displayAt);}	
 });
+}
+
+function loadList(columns)
+{
+	if(columns.length==0)
+	{
+	var options={
+	  type: 'person',
+	  client: myclient};
+	var collection=new Usergrid.Collection(options);
+	collection.fetch(function()
+		{
+		var prop=[];
+		for(var p in collection._list[0]._data)
+			if(p!='created'&&p!='metadata'&&p!='modified'&&p!='type'&&p!='uuid')
+			prop.push(p);
+		for(var i=0;i<prop.length;i++)
+		{
+			var opt=document.createElement('option');
+			opt.value=prop[i];
+			opt.innerHTML=prop[i];
+			columns.add(opt);
+		}
+		},
+		function(error) { if(error) console.log('could not find collection');} );
+	}
+}
+
+function FormQuery(col,op,qText)
+{
+	var theQuery="where "+col.value+" "+op.value+" '"+qText+"'";
+	return theQuery;
 }
 
